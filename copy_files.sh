ab8cd71b553d889410e8c8df335ce6100bef428d
Chris <61848401+cjm-64@users.noreply.github.com>
2024-04-15 11:30:53 -0400

    Delete copy_files.sh

--- copy_files.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-scp {globals,main,mainwindow,settime}.cpp {globals,mainwindow,settime}.h {mainwindow,settime}.ui BEAM.pro OpenKeyboard.sh beam@128.235.117.60:/home/beam/code/BEAM 
-
-ssh beam@128.235.117.60 "cd /home/beam/code/BEAM && qmake BEAM.pro && make && ls -la"
